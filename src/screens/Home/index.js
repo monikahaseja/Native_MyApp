a84deca2181624c23d69ae4889abd7a90fa01a65
Monika Haseja <monikahaseja@gmail.com>
2025-10-28 17:13:07 +0530

    Remove Class Component

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,27 +1,14 @@
-import React, { useEffect, useState } from "react";
 import { View } from "react-native";
-
+import React from "react";
 import Title from '../../components/Title';
-import TitleClass from '../../components/Title/classComponent';
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
-    const [title, setTitle] = useState('My First Component!!');
-
-    useEffect(() => {
-        setTimeout(() => {
-            setTitle('Updated prop text using timeout function.');
-        }, 3000);
-    }, []);
-
-    console.log('title:>>', title);
 
     return (
         <SafeAreaView>
             <View>
                 <Title />
-
-                <TitleClass text="Class Component" />
             </View>
         </SafeAreaView>
     );
